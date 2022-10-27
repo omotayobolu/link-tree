b15255e8fa6fc803b3996cb4d178b00f8038fe03
Boluwatife <omotayodaniel10@gmail.com>
2022-10-27 20:15:11 +0100

    links section done

---
+++ src/LinkData.js
@@ -0,0 +1,27 @@
+export const LinkData = [
+  {
+    id: "btn__zuri",
+    name: "Zuri Team",
+    link: "https://training.zuri.team/",
+  },
+  {
+    id: "books",
+    name: "Zuri Books",
+    link: " http://books.zuri.team",
+  },
+  {
+    id: "book__python",
+    name: "Python Books",
+    link: "https://books.zuri.team/python-for-beginners?ref_id=BoluOmotayo",
+  },
+  {
+    id: "pitch",
+    name: "Background Check for Coders",
+    link: "https://background.zuri.team",
+  },
+  {
+    id: "book__design",
+    name: "Design Books",
+    link: "https://books.zuri.team/design-rules",
+  },
+];
--- src/components/Links.jsx
+++ src/components/Links.jsx
@@ -1,7 +1,32 @@
 import React from "react";
 
+import { LinkData } from "../LinkData";
+import Slack from "../assets/slack.png";
+import Github from "../assets/github.png";
+
 const Links = () => {
-  return <main>Links</main>;
+  return (
+    <main className="mt-6">
+      {LinkData.map((link) => {
+        return (
+          <div className="flex flex-col my-4">
+            <button
+              id={link.id}
+              className="py-4 font-medium text-[18px] rounded-lg bg-grey disabled:bg-disabledGrey hover:bg-hoverGrey active:bg-activeGrey"
+            >
+              <a href={link.link} className="text-black leading-7">
+                {link.name}
+              </a>
+            </button>
+          </div>
+        );
+      })}
+      <div className="flex justify-center items-center gap-4 mt-8">
+        <img src={Slack} alt="" />
+        <img src={Github} alt="" />
+      </div>
+    </main>
+  );
 };
 
 export default Links;
